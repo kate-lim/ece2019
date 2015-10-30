de22e40e0d3cb739b1980e44a916e2bdcbc82bdd
Bilal Majeed <bilal.majeed@wishabi.com>
2015-10-29 22:30:44 -0400

    Added all files, created controllers, created views

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ece2019::Application.config.secret_token = '240420ae960e3fc1df4e9e7994773931a79fa870b57251c79f015ba3a44c5a128e3201503d9c9ca0ba4d7a1d381caea4278968753a3a7fdcbd4f05e949ec5d61'
---
+++ test/functional/classmates_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ClassmatesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
