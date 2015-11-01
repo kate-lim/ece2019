6b3bee3921825a9276d3915b39d9e84b3ab6d334
Bilal Majeed <bilal.majeed@wishabi.com>
2015-11-01 12:15:39 -0500

    fixied routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Ece2019::Application.routes.draw do
   resources :classmates
 
-  root :to => 'main#index'
+  root to: 'main#index'
   get '' => 'main#index'
   get '/home' => 'main#index'
   get '/courses' => 'courses#courses'
