b358e0d3488e5aada7d982adfaf58ee6456792bb
Bilal Majeed <bilalmajeed247@gmail.com>
2015-11-12 15:35:18 -0500

    fiz

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Ece2019
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['rails.js']
   end
 end
