7152fef6581094ae772cfb9f217c6b6119c0fa6a
Kate Lim <hotkoreanchick@hotmail.com>
2015-12-04 22:19:45 -0500

    -.- why won't you work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,18 +66,18 @@ Ece2019::Application.configure do
 
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'www.ece2019.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => "smtp.sendgrid.net",
-    :port => "587",
-    :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => "heroku.com",
-    :enable_starttls_auto => true
+    address: "smtp.sendgrid.net",
+    port: "587",
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    enable_starttls_auto: true
   }
 end
