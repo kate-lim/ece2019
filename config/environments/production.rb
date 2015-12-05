f0c5c21bb07c36b89a535613d7ffe37912909204
Kate Lim <hotkoreanchick@hotmail.com>
2015-12-04 22:14:41 -0500

    prod email pls work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,12 +72,12 @@ Ece2019::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: "587",
-    domain: "gmail.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_USERNAME_DEV'],
-    password: ENV['GMAIL_PASSWORD_DEV']
+    :address => "smtp.sendgrid.net",
+    :port => "587",
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => "heroku.com",
+    :enable_starttls_auto => true
   }
 end
