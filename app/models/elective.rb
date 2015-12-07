a1bb8a6b3551f22b9d621519521f5bce907e528c
Kate Lim <hotkoreanchick@hotmail.com>
2015-12-06 20:36:34 -0500

    bug fixed

--- app/models/elective.rb
+++ app/models/elective.rb
@@ -4,6 +4,6 @@ class Elective < ActiveRecord::Base
 	def update_electives_from_api
 		api = UWaterlooAPI.new('7467c4874254fc4f0ee969d9edaaada3')
 		course = api.courses.subject(self.subject).catalog_number(self.number)
-		self.update_attributes(:field1 => course.title, :units => course.units, :description => course.description)
+		self.update_attributes(:title => course.title, :units => course.units, :description => course.description)
 	end
 end
