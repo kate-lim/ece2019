39a982503299c656418f68f91b2f4a716cb33294
Kate Lim <hotkoreanchick@hotmail.com>
2016-01-15 10:27:31 -0500

    removed final exam schedule

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -56,39 +56,39 @@ class MainController < ApplicationController
 
 		@general_news = Array.new
 		@eng_news = Array.new
-		# @sys_news = Array.new
+		@sys_news = Array.new
 
 		Information.all.order(published_date: :desc).each do |info|
 			if info.category == 'news'
 				@general_news.push(info)
 			elsif info.category == 'engineering'
 				@eng_news.push(info)
-			# elsif info.category == 'information-systems-technology'
-			# 	@sys_news.push(info)
+			elsif info.category == 'information-systems-technology'
+				@sys_news.push(info)
 			end
 		end
 
 		@exam_schedule = Exam.all.order(:exam_date)
 
-		@first_monday = @exam_schedule.first.exam_date
-		while @first_monday.monday? == false do
-		   @first_monday -= 1
+		if !@exam_schedule.blank? 
+			@first_monday = @exam_schedule.first.exam_date
+			while @first_monday.monday? == false do
+			   @first_monday -= 1
+			end
+
+			@last_friday = @exam_schedule.last.exam_date
+			while @last_friday.friday? == false do
+			   @last_friday += 1
+			end
+
+			if @first_monday.month == @last_friday.month
+				@month_string = Date::MONTHNAMES[@first_monday.month]
+			else
+				@month_string = Date::MONTHNAMES[@first_monday.month] + " / " + Date::MONTHNAMES[@last_friday.month]
+			end
+
+			@days_in_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 		end
-
-		@last_friday = @exam_schedule.last.exam_date
-		while @last_friday.friday? == false do
-		   @last_friday += 1
-		end
-
-		if @first_monday.month == @last_friday.month
-			@month_string = Date::MONTHNAMES[@first_monday.month]
-		else
-			@month_string = Date::MONTHNAMES[@first_monday.month] + " / " + Date::MONTHNAMES[@last_friday.month]
-		end
-
-		
-
-		@days_in_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 	end
 
 end
--- app/models/information.rb
+++ app/models/information.rb
@@ -19,7 +19,7 @@ class Information < ActiveRecord::Base
 			end
 		end
 
-		date_criteria = Date.today - 10.days
+		date_criteria = Date.today - 1.months
 		general_news = api.news.site('news')
 		general_news.each do |article|
 			article_date = DateTime.iso8601(article.published).to_date
@@ -32,18 +32,18 @@ class Information < ActiveRecord::Base
 			end
 		end
 
-		# date_criteria = Date.today - 1.weeks
-		# sys_news = api.news.site('information-systems-technology')
-		# sys_news.each do |article|
-		# 	article_date = DateTime.iso8601(article.published).to_date
-		# 	if(article_date > date_criteria)
-		# 		formatted = {
-		# 			"site"		=> 'information-systems-technology',
-		# 			"article"	=> article
-		# 		}
-		# 		data_to_return.push(formatted)
-		# 	end
-		# end
+		date_criteria = Date.today - 2.weeks
+		sys_news = api.news.site('information-systems-technology')
+		sys_news.each do |article|
+			article_date = DateTime.iso8601(article.published).to_date
+			if(article_date > date_criteria)
+				formatted = {
+					"site"		=> 'information-systems-technology',
+					"article"	=> article
+				}
+				data_to_return.push(formatted)
+			end
+		end
 
 		return data_to_return
 	end
